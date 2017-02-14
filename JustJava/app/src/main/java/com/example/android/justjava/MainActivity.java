bf16ed9eaa814920efe0fb2f51eacedf55407613
Vicuko <victorc.fernandez@gmail.com>
2017-02-14 22:14:09 +0100

    Modify JustJava project to include additional 2 text views + function to update 1 of the new text views

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,9 +7,11 @@
 package com.example.android.justjava;
 
 import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.view.View;
-        import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -26,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        display(2);
+        displayPrice(2 * 5);
     }
 
     /**
@@ -36,4 +39,10 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
 }
