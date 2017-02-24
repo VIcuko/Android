ae8dfd1edf49b018f482b4df729ca647e3020b85
Vicuko <victorc.fernandez@gmail.com>
2017-02-24 20:46:36 +0100

    Generate log for each TextView including its text

--- LogTestApp/app/src/main/java/com/example/android/logtestapp/MainActivity.java
+++ LogTestApp/app/src/main/java/com/example/android/logtestapp/MainActivity.java
@@ -1,10 +1,10 @@
-package com.example.android.menu;
+package com.example.android.logtestapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-
-import com.example.android.logtestapp.R;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,10 +17,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void printToLogs(View view) {
         // Find first menu item TextView and print the text to the logs
-
+        TextView firstTextView = (TextView) findViewById(R.id.menu_item_1);
+        Log.i("First TextView content",(String) firstTextView.getText());
         // Find second menu item TextView and print the text to the logs
-
+        TextView secondTextView = (TextView) findViewById(R.id.menu_item_2);
+        Log.i("Second TextView content",(String) firstTextView.getText());
         // Find third menu item TextView and print the text to the logs
-
+        TextView thirdTextView = (TextView) findViewById(R.id.menu_item_3);
+        Log.i("Third TextView content",(String) firstTextView.getText());
     }
 }
