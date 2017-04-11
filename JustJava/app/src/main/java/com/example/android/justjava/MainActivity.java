b6059e84b548c86bcf1d3a8c4ff75071782172e1
Vicuko <victorc.fernandez@gmail.com>
2017-04-11 22:11:13 +0200

    add minimum and maximum limit to amount of coffee

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -10,7 +10,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -18,6 +20,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
+    int cupPrice = 5;
+    int whippedCreamPrice = 1;
+    int chocolatePrice = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,28 +34,44 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        //Find the user's name
+        EditText userNameText = (EditText) findViewById(R.id.name_text);
+        String userName = userNameText.getText().toString();
+
+        //Figure out if the user wants whipped cream topping
         CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean haswhippedCream = whippedCreamCheckbox.isChecked();
+
+        //Figure out if the user wants chocolate topping
         CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate= chocolateCheckbox.isChecked();
-        int price = calculatePrice(quantity, 5, haswhippedCream, hasChocolate);
-        String priceMessage = createOrderSummary(price, haswhippedCream, hasChocolate);
-        displayMessage(priceMessage);
-    }
 
-    /**
-     * This method is called when the plus button is clicked.
-     */
-    public void increment(View view) {
-        quantity += 1;
-        displayQuantity(quantity);
+        int price = calculatePrice(quantity, haswhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(price, haswhippedCream, hasChocolate, userName);
+        displayMessage(priceMessage);
     }
 
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
+        if (quantity<=1){
+            Toast.makeText(this, "The number of coffees can't be smaller than 1", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity -= 1;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        if (quantity>=100){
+            Toast.makeText(this, "The number of coffees can't be higher than 100", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity += 1;
         displayQuantity(quantity);
     }
 
@@ -84,9 +105,21 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param quantity is the number of cups of coffee ordered
      */
-    private int calculatePrice(int quantity, int cupPrice, boolean hasWhippedCream, boolean hasChocolate) {
-        int price = quantity * cupPrice;
-        return price;
+    private int calculatePrice(int quantity, boolean hasWhippedCream, boolean hasChocolate) {
+        int final_price=0;
+
+        // Add price of whipped cream
+        if (hasWhippedCream){
+            final_price = final_price + whippedCreamPrice;
+        }
+
+        // Add price of chocolate
+        if (hasChocolate){
+            final_price = final_price + chocolatePrice;
+        }
+        final_price = quantity * (cupPrice + final_price);
+
+        return final_price;
     }
 
     /**
@@ -96,8 +129,8 @@ public class MainActivity extends AppCompatActivity {
      * @param price of the order
      * @return the text summary
      */
-    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate) {
-        String summary = "Name: Jimmy Jones";
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate, String userName) {
+        String summary = "Name: " + userName;
         summary +="\nAdd whipped cream? " + hasWhippedCream;
         summary +="\nAdd chocolate? " + hasChocolate;
         summary += "\nQuantity: " + quantity;
